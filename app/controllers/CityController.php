f204c7c87857425285b8403f0338303eb5e1a9f3
daniel-rp-wagner <daniel-rp-wagner@web.de>
2025-03-11 20:29:01 +0100

    Multilanguage support by route

--- app/controllers/CityController.php
+++ app/controllers/CityController.php
@@ -2,23 +2,23 @@
 
 class CityController extends Controller {
     // Beispiel: Aufruf via Route: 'api/cities'
-    public function index() {
+    public function index($resourceId, $lang) {
         // Load the Book model
         $cityModel = $this->loadModel("City");
         // Retrieve the book with the given ID
-        $cities = $cityModel->getAllCities();
+        $cities = $cityModel->getAllCities($resourceId, $lang);
         header('Content-Type: application/json; charset=utf-8');
         echo json_encode($cities, JSON_UNESCAPED_UNICODE);
     }
 
     // Beispiel: Aufruf via Route: 'api/city/{id}'
-    public function cityById($resourceId) {
+    public function cityById($resourceId, $lang) {
         // echo "CityController: cityById method - resourceId: " . $resourceId;
 
         // Load the Book model
         $cityModel = $this->loadModel("City");
         // Retrieve the book with the given ID
-        $city = $cityModel->getCityById($resourceId);
+        $city = $cityModel->getCityById($resourceId, $lang);
         header('Content-Type: application/json; charset=utf-8');
         echo json_encode($city, JSON_UNESCAPED_UNICODE);
     }
