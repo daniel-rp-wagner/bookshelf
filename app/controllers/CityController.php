f043a79e3a8ac9bfbf5a06da3bd74078123adb3a
daniel-rp-wagner <daniel-rp-wagner@web.de>
2025-03-12 20:17:03 +0100

    first try to create city

--- app/controllers/CityController.php
+++ app/controllers/CityController.php
@@ -1,20 +1,18 @@
 <?php
 
 class CityController extends Controller {
-    // Beispiel: Aufruf via Route: 'api/cities'
+    // Beispiel: Aufruf via Route: 'api/fr/cities'
     public function index($resourceId, $lang) {
         // Load the Book model
         $cityModel = $this->loadModel("City");
         // Retrieve the book with the given ID
-        $cities = $cityModel->getAllCities($resourceId, $lang);
+        $cities = $cityModel->getAllCities($lang);
         header('Content-Type: application/json; charset=utf-8');
         echo json_encode($cities, JSON_UNESCAPED_UNICODE);
     }
 
-    // Beispiel: Aufruf via Route: 'api/city/{id}'
+    // Beispiel: Aufruf via Route: 'api/fr/city/{id}'
     public function cityById($resourceId, $lang) {
-        // echo "CityController: cityById method - resourceId: " . $resourceId;
-
         // Load the Book model
         $cityModel = $this->loadModel("City");
         // Retrieve the book with the given ID
@@ -24,11 +22,33 @@ class CityController extends Controller {
     }
 
     // Beispiel: Aufruf via Route: 'api/city/add'
-    public function addNewCity($resourceId = 0) {
-        echo "CityController: addNewCity method";
-        if ($resourceId) {
-            echo " - resourceId: " . $resourceId;
-        }
+    public function addNewCity($lang) {
+        // echo "CityController: addNewCity method";
+        $data = array(
+            'id' => 1234567, 
+            'country_iso' => 'DE', 
+            'parent_city_id' => null, 
+            'type' => 'city',
+            'coordinates' => array(
+                'latitude' => 1.1,
+                'longitude' => 2.2
+            ),
+            'names' => array(
+                array(
+                    'name' => 'Stadt1', 
+                    'language_code' => 'on'
+                ),
+                array(
+                    'name' => 'Stadt2', 
+                    'language_code' => 'fr'
+                )
+            )
+        );
+
+         // Load the Book model
+        $cityModel = $this->loadModel("City");
+        // Retrieve the book with the given ID
+        $city = $cityModel->createCity($data);
     }
 
     // Beispiel: Aufruf via Route: 'api/city/delete/{id}'
