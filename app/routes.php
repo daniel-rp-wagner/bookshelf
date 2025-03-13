1e9228a4491a65e8fd9383b370707f8ad0c28f08
daniel-rp-wagner <daniel-rp-wagner@web.de>
2025-03-13 13:07:28 +0100

    routes: best practices

--- app/routes.php
+++ app/routes.php
@@ -2,17 +2,17 @@
 
 // Define an array of routes for the application
 $routes = [
-	'index'                         	   => ['method' => 'GET', 'controller' => 'HomeController',         'action' => 'index'],
+	['route' => 'index', 'method' => 'GET', 'controller' => 'HomeController',         'action' => 'index'],
 	
-	'api/{lang}/cities'                    => ['method' => 'GET', 'controller' => 'CityController',         'action' => 'index'],
-	'api/{lang}/city/{id}'                 => ['method' => 'GET', 'controller' => 'CityController',         'action' => 'cityById'],
-	'api/{lang}/city/add'                  => ['method' => 'POST', 'controller' => 'CityController',         'action' => 'addNewCity'],
-	'api/{lang}/city/delete/{id}'          => ['method' => 'DELETE', 'controller' => 'CityController',         'action' => 'deleteCity'],
-	'api/{lang}/city/update/{id}'          => ['method' => 'PUT', 'controller' => 'CityController',         'action' => 'updateCity'],
+	['route' => 'api/{lang}/cities', 'method' => 'GET', 'controller' => 'CityController',         'action' => 'index'],
+	['route' => 'api/{lang}/cities', 'method' => 'POST', 'controller' => 'CityController',         'action' => 'addNewCity'],
+	['route' => 'api/{lang}/cities/{id}', 'method' => 'GET', 'controller' => 'CityController',         'action' => 'cityById'],
+	['route' => 'api/{lang}/cities/{id}', 'method' => 'DELETE', 'controller' => 'CityController',         'action' => 'deleteCity'],
+	['route' => 'api/{lang}/cities/{id}', 'method' => 'PUT', 'controller' => 'CityController',         'action' => 'updateCity'],
 	
-	'api/{lang}/organizations'             => ['method' => 'GET', 'controller' => 'OrganizationController', 'action' => 'index'],
-	'api/{lang}/organization/{id}'         => ['method' => 'GET', 'controller' => 'OrganizationController', 'action' => 'organizationById'],
-	'api/{lang}/organization/add'          => ['method' => 'POST', 'controller' => 'OrganizationController', 'action' => 'addNewOrganization'],
-	'api/{lang}/organization/delete/{id}'  => ['method' => 'DELETE', 'controller' => 'OrganizationController', 'action' => 'deleteOrganization'],
-	'api/{lang}/organization/update/{id}'  => ['method' => 'PUT', 'controller' => 'OrganizationController', 'action' => 'updateOrganization']
+	['route' => 'api/{lang}/organizations', 'method' => 'GET', 'controller' => 'OrganizationController', 'action' => 'index'],
+	['route' => 'api/{lang}/organizations', 'method' => 'POST', 'controller' => 'OrganizationController', 'action' => 'addNewOrganization'],
+	['route' => 'api/{lang}/organizations/{id}', 'method' => 'GET', 'controller' => 'OrganizationController', 'action' => 'organizationById'],
+	['route' => 'api/{lang}/organizations/{id}', 'method' => 'DELETE', 'controller' => 'OrganizationController', 'action' => 'deleteOrganization'],
+	['route' => 'api/{lang}/organizations/{id}', 'method' => 'PUT', 'controller' => 'OrganizationController', 'action' => 'updateOrganization']
 ];
