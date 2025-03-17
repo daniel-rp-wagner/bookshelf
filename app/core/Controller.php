246a7443317b23aac9e741ff3c12acbca0bea059
daniel-rp-wagner <daniel-rp-wagner@web.de>
2025-03-17 08:22:08 +0100

    comments and types

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -17,7 +17,7 @@ class Controller
      * @param string $model The name of the model to load.
      * @return object An instance of the specified model.
      */
-    protected function loadModel(string $model)
+    protected function loadModel(string $model): object
     {
         require_once '../app/models/' . $model . '.php';
         return new $model;
@@ -26,14 +26,14 @@ class Controller
     /**
      * Renders the view.
      *
-     * This method loads the layout file which is responsible for rendering the view.
+     * This method loads the layout file, which is responsible for rendering the view.
      * Optional data and title can be passed to be used within the view.
      *
      * @param mixed $data Optional data to be passed to the view.
      * @param string $title The title for the view. Default is "Shelf".
      * @return void
      */
-    protected function renderView($data = '', string $title = "Shelf"): void
+    protected function renderView(mixed $data = '', string $title = "Shelf"): void
     {
         require_once '../app/views/layout.php';
     }
@@ -46,19 +46,29 @@ class Controller
      * @param mixed $data The data to output as JSON.
      * @return void
      */
-    protected function outputData($data = ''): void
+    protected function outputData(mixed $data = ''): void
     {
         header('Content-Type: application/json; charset=utf-8');
         echo json_encode($data, JSON_UNESCAPED_UNICODE);
     }
 
-    protected function pagination(int $size, int $page){
+    /**
+     * Generates an SQL pagination clause based on the provided size and page number.
+     *
+     * If both size and page are greater than 0, it calculates the LIMIT and OFFSET values.
+     * Otherwise, an empty string is returned.
+     *
+     * @param int $size The number of records per page.
+     * @param int $page The current page number.
+     * @return string The SQL pagination clause (e.g., " LIMIT 10 OFFSET 20").
+     */
+    protected function pagination(int $size, int $page): string
+    {
         $query = '';
 
-        if($size > 0 && $page > 0){
+        if ($size > 0 && $page > 0) {
             $limit = $size;
-            $offset = $page > 1 ? ($size * ($page-1)) : 0;
-            
+            $offset = $page > 1 ? ($size * ($page - 1)) : 0;
             $query = ' LIMIT ' . $limit . ' OFFSET ' . $offset;
         }
 
