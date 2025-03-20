a1ffba529034eab71e5ca43d5b6e6953f918a2df
daniel-rp-wagner <daniel-rp-wagner@web.de>
2025-03-20 08:13:29 +0100

    set_exeption_header

--- public/index.php
+++ public/index.php
@@ -4,5 +4,12 @@ require_once "../app/core/Database.php";
 require_once "../app/core/Controller.php";
 require_once "../app/core/App.php";
 
+set_exception_handler(function (Throwable $exception): void {
+    http_response_code(500);
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode(['error' => $exception->getMessage()]);
+    exit;
+});
+
 // Create a new instance of the App class
 $app = new App();
