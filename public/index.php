9f9125297d8b104a02fbb5d580bbe6872f8db533
daniel-rp-wagner <daniel-rp-wagner@web.de>
2025-03-21 06:58:15 +0100

    add ApiException-Class

--- public/index.php
+++ public/index.php
@@ -1,15 +1,9 @@
 <?php
 require_once "../app/config/config.php";
+require_once "../app/core/ApiException.php";
 require_once "../app/core/Database.php";
 require_once "../app/core/Controller.php";
 require_once "../app/core/App.php";
-
-set_exception_handler(function (Throwable $exception): void {
-    http_response_code(500);
-    header('Content-Type: application/json; charset=utf-8');
-    echo json_encode(['error' => $exception->getMessage()]);
-    exit;
-});
 
 // Create a new instance of the App class
 $app = new App();
